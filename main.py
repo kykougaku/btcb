d254fe0ec25ee632a894251e640cbf464fec24c7
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-10-24 16:55:02 +0900

    updated

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+btcjpyticker = yf.Ticker("BTC-JPY")
+
+hist = btcjpyticker.history(period="1mo",interval="1h")
+
+print(hist)
+print(hist.columns)
+print(hist.index)
+print(type(hist))
+plt.plot(hist.Close)
+plt.show()
