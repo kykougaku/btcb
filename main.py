ea1cd13cb63ad1aa303f2aae1da9233fc48e58cd
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-10-25 16:47:18 +0900

    intersection is pointed

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ hist["Signal"] = hist["MACD"].ewm(span=9).mean()
 
 
 hist["g_point"] = False
+hist["s_point"] = False
 for i in range(len(hist.index)-1):
     hist["g_point"].iat[i+1] = False
     if(hist.MACD[i]<hist.Signal[i] and hist.MACD[i+1]>hist.Signal[i+1]):
         hist["g_point"].iat[i+1] = True
-
-
-
+    elif(hist.MACD[i]>hist.Signal[i] and hist.MACD[i+1]<hist.Signal[i+1]):
+        hist["s_point"].iat[i+1] = True
 
 
 fig = plt.figure()
@@ -39,7 +39,17 @@ up.plot(hist.index,hist["EMA5"],color="green")
 up.plot(hist.index,hist["SMA25"],color="orange")
 down.plot(hist.index,hist["MACD"],color="blue")
 down.plot(hist.index,hist["Signal"],color="red")
+
+for i in range(len(hist.index)):
+    if(hist.g_point[i]):
+        up.scatter(hist.index[i],hist["Close"][i],facecolor="none",edgecolors="blue",)
+        down.scatter(hist.index[i],hist["MACD"][i],facecolor="none",edgecolors="blue")
+    elif(hist.s_point[i]):
+        up.scatter(hist.index[i],hist["Close"][i],facecolor="none",edgecolors="red",)
+        down.scatter(hist.index[i],hist["MACD"][i],facecolor="none",edgecolors="red")
+
 up.legend(["Close","EMA5"])
 down.legend(["MACD","Signal"])
 
+plt.savefig("fig.png",dpi=3000)
 plt.show()
