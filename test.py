ab1b732e1d49e68a776f129bb7555ba8663d6353
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-11-04 23:32:40 +0900

    colormapp

--- test.py
+++ test.py
@@ -12,23 +12,28 @@ hist["Datetime"] = pd.to_datetime(hist["Datetime"])
 hist.set_index("Datetime",inplace=True)
 
 print(hist.head())
-spread = 0.9999
+spread = 0.999
 yen_assets = 50000.0
 btc_assets_yen = 50000.0
 
 s = 13
 l = 29
 sig = 14
+sma = 25
+ema = 25
 
 
 hist["EMA"+str(s)] = hist["Close"].ewm(span=s).mean()
 hist["EMA"+str(l)] = hist["Close"].ewm(span=l).mean()
-hist["SMA25"] = hist["Close"].rolling(25).mean()
+hist["SMA"+str(sma)] = hist["Close"].rolling(sma).mean()
+hist["EMA"+str(ema)] = hist["Close"].ewm(span=ema).mean()
 hist["MACD"] = hist["EMA"+str(s)] - hist["EMA"+str(l)]
 hist["Signal"] = hist["MACD"].ewm(span=sig).mean()
 
 hist["before_MACD"] = hist["MACD"].shift()
 hist["before_Signal"] = hist["Signal"].shift()
+hist["before_SMA"+str(sma)] = hist["SMA"+str(sma)].shift()
+hist["before_EMA"+str(ema)] = hist["EMA"+str(ema)].shift()
 
 hist["upper_sigma4"], _, hist["lower_sigma4"] = ta.BBANDS(hist["Close"], timeperiod=25, nbdevup=4, nbdevdn=4, matype=ta.MA_Type.SMA)
 
@@ -40,18 +45,24 @@ s_point =[]
 yens = []
 btcs = []
 
+smas = []
+emas = [] 
+closes = []
 macd_grads = []
 signal_grads = []
+sma_grads = []
+ema_grads = []
 profits = []
 
 yen = yen_assets
 btc = btc_assets_yen/ hist["Close"].iloc[0]
-for idx,(macd,signal,before_macd,before_signal,close,upsigma4,losigma4) in enumerate(zip(hist["MACD"].values,hist["Signal"].values,hist["before_MACD"].values,hist["before_Signal"].values,hist["Close"].values,hist["upper_sigma4"].values,hist["lower_sigma4"].values)):
+for idx,(macd,signal,before_macd,before_signal,close,upsigma4,losigma4,smass,emass,before_smass,before_emass) in enumerate(zip(hist["MACD"].values,hist["Signal"].values,hist["before_MACD"].values,hist["before_Signal"].values,hist["Close"].values,hist["upper_sigma4"].values,hist["lower_sigma4"].values,hist["SMA"+str(sma)].values,hist["EMA"+str(ema)].values,hist["before_SMA"+str(sma)].values,hist["before_EMA"+str(ema)].values)):
     if(close<losigma4 or upsigma4<close):
         mode = 1
     elif(losigma4<close and close<upsigma4 and mode==1):
         mode = 2
 
+    #if(before_macd<before_signal and macd>signal and (-70000.0>(emass-close) or (emass-close)>100000) and mode==2): 
     if(before_macd<before_signal and macd>signal and mode==2): 
         g_point.append(True)
         s_point.append(False)
@@ -60,6 +71,11 @@ for idx,(macd,signal,before_macd,before_signal,close,upsigma4,losigma4) in enume
 
         macd_grads.append(macd-before_macd)
         signal_grads.append(signal-before_signal)
+        smas.append(smass)
+        emas.append(emass)
+        sma_grads.append(smass-before_smass)
+        ema_grads.append(emass-before_emass)
+        closes.append(close)
         tempo = btc*close
     elif(before_macd>before_signal and macd<signal and mode == 3):
         g_point.append(False)
@@ -89,7 +105,8 @@ down = fig.add_subplot(3,1,2)
 down2 = fig.add_subplot(3,1,3)
 
 up.plot(hist.index,hist["Close"],color="black")
-up.plot(hist.index,hist["SMA25"],color="orange")
+up.plot(hist.index,hist["SMA"+str(sma)],color="orange")
+up.plot(hist.index,hist["EMA"+str(ema)],color="green")
 up.plot(hist.index,hist["lower_sigma4"],color="red")
 down.plot(hist.index,hist["MACD"],color="blue")
 down.plot(hist.index,hist["Signal"],color="red")
@@ -102,19 +119,24 @@ for i in range(len(hist.index)):
         up.scatter(hist.index[i],hist.loc[:,'Close'].iloc[i],facecolor="none",edgecolors="red",)
         down.scatter(hist.index[i],hist.loc[:,'MACD'].iloc[i],facecolor="none",edgecolors="red")
 
-up.legend(["Close","SMA25"])
+up.legend(["Close","SMA"+str(sma),"EMA"+str(ema),"lower_sigma4"])
 down.legend(["MACD","Signal"])
 
 down.text(hist.index[0],5000,"s="+str(s)+"\nl="+str(l)+'\nsignal='+str(sig),fontsize=10)
 
-plt.savefig("fig"+str(s)+"-"+str(l)+"-"+str(sig)+".png",dpi=300)
-plt.show()
-grad_datas = pd.DataFrame(list(zip(macd_grads,signal_grads,profits)),columns=["MACD_grad","Signal_grad","profit"])
-fig2 = plt.figure()
-plt.scatter(grad_datas["MACD_grad"],grad_datas["Signal_grad"],c=grad_datas["profit"],cmap="jet")
-plt.colorbar()
+plt.savefig("fig"+str(s)+"-"+str(l)+"-"+str(sig)+".png",dpi=800)
 plt.show()
 
+grad_datas = pd.DataFrame(list(zip(macd_grads,signal_grads,profits,smas,emas,closes,sma_grads,ema_grads)),columns=["MACD_grad","Signal_grad","profit","SMA"+str(sma),"EMA"+str(ema),"Close","SMA_grad","EMA_grad"])
+
 fig3 = plt.figure()
-plt.scatter(grad_datas["MACD_grad"]*((grad_datas["MACD_grad"]-grad_datas["Signal_grad"])**2.0),grad_datas["profit"])
+#plt.scatter(grad_datas["MACD_grad"]*((grad_datas["MACD_grad"]-grad_datas["Signal_grad"])**2.0),grad_datas["profit"])
+plt.scatter(grad_datas["EMA"+str(ema)]-grad_datas["Close"],grad_datas["profit"])
+plt.show()
+fig4 = plt.figure()
+plt.scatter(grad_datas["EMA_grad"],grad_datas["profit"])
+plt.show()
+fig5 = plt.figure()
+plt.scatter(grad_datas["EMA"+str(ema)]-grad_datas["Close"],grad_datas["SMA_grad"],c=grad_datas["profit"],cmap="jet")
+plt.colorbar()
 plt.show()
