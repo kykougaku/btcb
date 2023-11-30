88d28eef2447387e9d6684727367a8d230c95bd4
kykougaku <6489550+kykougaku@users.noreply.github.com>
2023-11-30 22:59:38 +0900

    updated

--- ml/ml.py
+++ ml/ml.py
@@ -3,11 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import talib as ta
 
-import sklearn
+import lightgbm as lgb
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 
 df = pd.read_csv("btcjpy_data_from_yfinance-2y-1d.csv")
 df["diff"] = df["Close"] - df["Open"]
 df["diff_ratio"] = df["diff"] / df["Open"]
+
+print(df["diff_ratio"].sum())
 
 plt.hist(df["diff_ratio"], bins=100)
 plt.show()
